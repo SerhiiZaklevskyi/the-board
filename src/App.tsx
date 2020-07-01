3ca143aa8e0620926c500c3e7d6e104c28f6b6b0
SerhiiZaklevskyi <serhii.jh@gmail.com>
2020-07-01 12:55:14 +0300

    added normalize

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux'
 import ApplicationStore from './store'
 import Wrapper from './components/wrapper/wrapper'
 import GlobalStyles from './common/styles/global'
+import { Normalize } from 'styled-normalize'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <React.Fragment>
+      <Normalize />
       <GlobalStyles />
       <ToastContainer toastClassName='toast' />
       <Provider store={ApplicationStore}>
